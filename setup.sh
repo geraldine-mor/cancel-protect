a61ea1cfb02d0a251171ff55073e24c4b9aabf7b
Geraldine Morey <geraldinemorey@rocketmail.com>
2026-08-03 19:00:43 +0100

    Add debug messages to setup.sh to see if it is functioning at all

--- setup.sh
+++ setup.sh
@@ -1,16 +1,14 @@
-mkdir -p ~/.streamlit/
+mkdir -p ~/.streamlit
+
+echo "SETUP RAN" >&2
 
 cat > ~/.streamlit/config.toml <<EOF
 [server]
 headless = true
 port = $PORT
-enableCORS = false
 address = "0.0.0.0"
-
-[theme]
-primaryColor = "#1B4F8C"
-backgroundColor = "#F5F7FA"
-secondaryBackgroundColor = "#DCE3EC"
-textColor = "#14213D"
-font = "sans serif"
+enableCORS = false
 EOF
+
+echo "CONFIG CREATED" >&2
+cat ~/.streamlit/config.toml >&2
