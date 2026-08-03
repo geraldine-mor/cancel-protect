82accbd7998ac01ed626e08d53a21a189cc8640b
Geraldine Morey <geraldinemorey@rocketmail.com>
2026-08-03 17:04:18 +0100

    Add theme config to the setup.sh file and remove conflicting confile file

--- setup.sh
+++ setup.sh
@@ -5,4 +5,10 @@ headless = true\n\
 port = $PORT\n\
 enableCORS = false\n\
 \n\
+[theme]\n\
+primaryColor = "#1B4F8C"\n\           
+backgroundColor = "#F5F7FA"\n\        
+secondaryBackgroundColor = "#DCE3EC"\n\  
+textColor = "#14213D"\n\
+font = "sans serif"\n\
 " > ~/.streamlit/config.toml
